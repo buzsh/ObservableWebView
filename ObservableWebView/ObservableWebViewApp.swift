63de259ca89ecf8149ba4053468effb16781a297
Justin Bush <jubush@icloud.com>
2024-03-15 09:21:01 -0600

    feat: SwiftUI project

---
+++ ObservableWebView/ObservableWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableWebViewApp.swift
+//  ObservableWebView
+//
+//  Created by Justin Bush on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservableWebViewApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
