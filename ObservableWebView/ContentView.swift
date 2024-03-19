9ca581b1eedbc8d00a983461295a25e4423534a0
Justin Bush <jdevbush@gmail.com>
2024-03-19 11:17:20 -0600

    refactor: CustomizableBrowserToolbar.swift

--- ObservableWebView/ContentView.swift
+++ ObservableWebView/ContentView.swift
@@ -104,34 +104,3 @@ extension ContentView {
     }
   }
 }
-
-import SwiftUI
-
-struct CustomizableBrowserToolbar: ToolbarContent, CustomizableToolbarContent {
-  let manager: ObservableWebViewManager
-  @Binding var toolbarStringText: String
-  
-  var body: some CustomizableToolbarContent {
-    ToolbarItem(id: "backButton", placement: .automatic) {
-      ToolbarSymbolButton(title: "Back", symbol: .back, action: manager.goBack)
-        .disabled(!manager.canGoBack)
-    }
-    
-    ToolbarItem(id: "forwardButton", placement: .automatic) {
-      ToolbarSymbolButton(title: "Forward", symbol: .forward, action: manager.goForward)
-        .disabled(!manager.canGoForward)
-    }
-    
-    ToolbarItem(id: "urlSearchBar", placement: .automatic) {
-      TextField("Search or type URL", text: $toolbarStringText)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-        .frame(minWidth: 150, maxWidth: 300)
-        .onSubmit {
-          manager.load(toolbarStringText)
-        }
-        .onChange(of: manager.urlString) {
-          toolbarStringText = manager.urlString
-        }
-    }
-  }
-}
