bbca1cbfe92cf4ad84643b375e6ee91af5731f25
Justin Bush <jdevbush@gmail.com>
2024-04-06 12:17:23 -0600

    feat: ViewExtensions

---
+++ ObservableWebView/BrowserViews/Extensions/ViewExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  ViewExtensions.swift
+//  ObservableWebView
+//
+//  Created by Justin Bush on 4/6/24.
+//
+
+import SwiftUI
+
+extension View {
+  func animateOnChange<T: Equatable>(of value: T, with state: Binding<T>) -> some View {
+    self.onChange(of: value) {
+      withAnimation {
+        state.wrappedValue = value
+      }
+    }
+  }
+}
